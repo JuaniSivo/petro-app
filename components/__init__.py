460e9ded41b99c695de313abbc397fd751eeb331
JuaniSivo <juan_ignacio_sivori@yahoo.com.ar>
2026-03-22 22:25:35 +0100

    chore: initialise petro-app repo skeleton

---
+++ components/__init__.py
@@ -0,0 +1,4 @@
+from components.uncertain_input import uncertain_input
+from components.plots import plot_prob_array, plot_histogram, plot_retrospective
+
+__all__ = ["uncertain_input", "plot_prob_array", "plot_histogram", "plot_retrospective"]
---
+++ project/__init__.py
@@ -0,0 +1,4 @@
+from project.state import Project
+from project.io import save_project, load_project
+
+__all__ = ["Project", "save_project", "load_project"]
